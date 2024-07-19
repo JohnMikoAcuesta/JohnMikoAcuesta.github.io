43bc2e0a72a2869f32a9253e41a78b2407a9e657
John Miko Acuesta <112823181+JohnMikoAcuesta@users.noreply.github.com>
2024-07-19 12:03:18 +0400

    Added Visitor Counter Code

--- public/source/main.js
+++ public/source/main.js
@@ -19,3 +19,23 @@ btnScrollToTop.addEventListener("click", e => {
 window.addEventListener('scroll', e => {
   btnScrollToTop.style.display = window.scrollY > 20 ? 'block' : 'none';
 });
+
+var counterContainer = document.querySelector(".counter");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
